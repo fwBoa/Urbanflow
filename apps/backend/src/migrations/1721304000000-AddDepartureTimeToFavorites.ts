9eea862537b71f9ae11f4db12599337ca30f4985
Jean-David Zamblezie <jeandavidzamblezie@outlook.fr>
2026-07-19 18:31:54 +0200

    style(db): corrige le formatage prettier de la migration

--- apps/backend/src/migrations/1721304000000-AddDepartureTimeToFavorites.ts
+++ apps/backend/src/migrations/1721304000000-AddDepartureTimeToFavorites.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddDepartureTimeToFavorites1721304000000
-  implements MigrationInterface
-{
+export class AddDepartureTimeToFavorites1721304000000 implements MigrationInterface {
   name = 'AddDepartureTimeToFavorites1721304000000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
